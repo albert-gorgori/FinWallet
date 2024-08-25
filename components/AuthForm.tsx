ec75b177342d278be6a93587da4088768c9bf44e
AlbertGorgori <albert.gorgori@outlook.com>
2024-08-25 07:03:34 +0200

    Sign up form

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -26,8 +26,10 @@ const AuthForm = ({ type }: { type: string }) => {
   const [user, setUser] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
 
-  const form = useForm<z.infer<typeof authFormSchema>>({
-    resolver: zodResolver(authFormSchema),
+  const formSchema = authFormSchema(type);
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
     defaultValues: {
       email: "",
       password: "",
@@ -35,7 +37,7 @@ const AuthForm = ({ type }: { type: string }) => {
   });
 
   // 2. Define a submit handler.
-  function onSubmit(values: z.infer<typeof authFormSchema>) {
+  function onSubmit(values: z.infer<typeof formSchema>) {
     setIsLoading(true);
     console.log(values);
     setIsLoading(false);
@@ -72,6 +74,58 @@ const AuthForm = ({ type }: { type: string }) => {
         <>
           <Form {...form}>
             <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+              {type === "sign-up" && (
+                <>
+                  <div className="flex gap-4">
+                    <CustomInput
+                      control={form.control}
+                      name={"firstName"}
+                      label={"First Name"}
+                      placeholder={"Enter your first name"}
+                    />
+                    <CustomInput
+                      control={form.control}
+                      name={"lastName"}
+                      label={"Last Name"}
+                      placeholder={"Enter your last name"}
+                    />
+                  </div>
+                  <CustomInput
+                    control={form.control}
+                    name={"address1"}
+                    label={"Address"}
+                    placeholder={"Enter your specific address"}
+                  />
+                  <div className="flex gap-4">
+                    <CustomInput
+                      control={form.control}
+                      name={"state"}
+                      label={"State"}
+                      placeholder={"Example: FL"}
+                    />
+                    <CustomInput
+                      control={form.control}
+                      name={"postalCode"}
+                      label={"Postal Code"}
+                      placeholder={"Example: 12345"}
+                    />
+                  </div>
+                  <div className="flex gap-4">
+                    <CustomInput
+                      control={form.control}
+                      name={"dateOfBirth"}
+                      label={"Date of Birth"}
+                      placeholder={"YYYY-MM-DD"}
+                    />
+                    <CustomInput
+                      control={form.control}
+                      name={"ssn"}
+                      label={"SSN"}
+                      placeholder={"Example: 1234"}
+                    />
+                  </div>
+                </>
+              )}
               <CustomInput
                 control={form.control}
                 name={"email"}
--- lib/utils.ts
+++ lib/utils.ts
@@ -195,15 +195,23 @@ export const getTransactionStatus = (date: Date) => {
   return date > twoDaysAgo ? "Processing" : "Success";
 };
 
-
-export const authFormSchema = z.object({
-  email: z.string().email({
-    message: "Please enter a valid email address.",
-  }),
-  username: z.string().min(3, {
-    message: "Username must be at least 3 characters long.",
-  }),
-  password: z.string().min(8, {
-    message: "Password must be at least 8 characters long.",
-  }),
-});
+export const authFormSchema = (type: string) =>
+  z.object({
+    //both sign in and sign up
+    email: z.string().email({
+      message: "Please enter a valid email address.",
+    }),
+    password: z.string().min(8, {
+      message: "Password must be at least 8 characters long.",
+    }),
+    //sign up fields
+    firstName: type === "sign-in" ? z.string().optional() : z.string().min(3),
+    lastName: type === "sign-in" ? z.string().optional() : z.string().min(3),
+    address1: type === "sign-in" ? z.string().optional() : z.string().max(50),
+    state:
+      type === "sign-in" ? z.string().optional() : z.string().min(2).max(2),
+    postalCode:
+      type === "sign-in" ? z.string().optional() : z.string().min(3).max(6),
+    dateOfBirth: type === "sign-in" ? z.string().optional() : z.string().min(3),
+    ssn: type === "sign-in" ? z.string().optional() : z.string().min(3),
+  });
