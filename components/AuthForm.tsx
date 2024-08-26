2a65fe6a9f04078ff0d3e1861a8dfeed5ad4b940
AlbertGorgori <albert.gorgori@outlook.com>
2024-08-26 05:59:10 +0200

    App wirte config - WIP

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -22,6 +22,7 @@ import CustomInput from "./CustomInput";
 import { authFormSchema } from "@/lib/utils";
 import { Loader2 } from "lucide-react";
 import { useRouter } from "next/navigation";
+import { signIn, signUp } from "@/lib/actions/user.actions";
 
 const AuthForm = ({ type }: { type: string }) => {
     const router = useRouter();
@@ -46,17 +47,17 @@ const AuthForm = ({ type }: { type: string }) => {
       // Sign up with Appwrite & create plain link token
 
       if (type === "sign-up") {
-        // const newUser = await SignUp(data);
-        // setUser(newUser);
+        const newUser = await signUp(data);
+        setUser(newUser);
       }
 
       if (type === "sign-in") {
-        // const response = await signIn({
-        //     email: data.email,
-        //     password: data.password,
-        // });
+        const response = await signIn({
+            email: data.email,
+            password: data.password,
+        });
 
-        // if (response) router.push("/");
+        if (response) router.push("/");
 
       }
     } catch (error) {
---
+++ lib/actions/user.actions.ts
@@ -0,0 +1,20 @@
+"use server";
+
+export const signIn = async (data: LoginUser) => {
+  try {
+    //Mutation / Database / Make fetch
+
+  } catch (error) {
+    console.error('Error', error);
+  }
+};
+
+export const signUp = async (userData: SignUpParams) => {
+    try {
+      //Create a user account
+  
+    } catch (error) {
+      console.error('Error', error);
+    }
+  };
+  
