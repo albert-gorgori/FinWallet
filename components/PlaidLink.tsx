bdc8385e890d1c86fbbebade1522412d2a10eda5
AlbertGorgori <albert.gorgori@outlook.com>
2024-08-31 05:56:14 +0200

    Plaid scaffolding - wip

--- components/PlaidLink.tsx
+++ components/PlaidLink.tsx
@@ -1,11 +1,38 @@
-import React from "react";
+import { useCallback, useEffect, useState } from "react";
+
 import { Button } from "./ui/button";
+import { PlaidLinkOptions } from "react-plaid-link";
+import { StyledString } from "next/dist/build/swc";
+import { useRouter } from "next/router";
 
 const PlaidLink = ({ user, variant }: PlaidLinkProps) => {
+  const [token, setToken] = useState("");
+
+  const router = useRouter();
+
+  useEffect(() => {
+    const getLinkToken = async () => {
+      //   const data = await createLinkToken(user);
+      //   setToken(data?.linkToken);
+    };
+    getLinkToken();
+  }, []);
+
+  const onSuccess = useCallback(
+    async (public_token: StyledString) => {
+    //   await exchangePublicToken({ publicToken: public_token, user });
+        router.push("/");
+    },
+    [user]
+  );
+  const config: PlaidLinkOptions = {
+    token,
+    onSuccess,
+  };
   return (
     <>
       {variant === "primary" ? (
-        <Button>Connect Bank</Button>
+        <Button className="plaidlink-primary">Connect Bank</Button>
       ) : variant === "ghost" ? (
         <Button>Connect bank</Button>
       ) : (
