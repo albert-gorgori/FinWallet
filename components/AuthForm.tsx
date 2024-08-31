5f484d3dd41d81dd946f62eaa37e50d3b652df11
AlbertGorgori <albert.gorgori@outlook.com>
2024-08-31 05:34:16 +0200

    Plaid button - wip

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -91,11 +91,11 @@ const AuthForm = ({ type }: { type: string }) => {
           </p>
         </div>
       </header>
-      {user ? (
+      {/* {user ? ( */}
         <div className="flex flex-col gap-4">
           <PlaidLink user={user} variant="primary" />
         </div>
-      ) : (
+       {/* ) : ( */}
         <>
           <Form {...form}>
             <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
@@ -200,7 +200,7 @@ const AuthForm = ({ type }: { type: string }) => {
             </Link>
           </footer>
         </>
-      )}
+      {/* )} */}
     </section>
   );
 };
--- components/PlaidLink.tsx
+++ components/PlaidLink.tsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
+import { Button } from "./ui/button";
 
-const PlaidLink = ({user, variant}: PlaidLinkProps) => {
+const PlaidLink = ({ user, variant }: PlaidLinkProps) => {
   return (
-    <></>
-  )
-}
+    <>
+      {variant === "primary" ? (
+        <Button>Connect Bank</Button>
+      ) : variant === "ghost" ? (
+        <Button>Connect bank</Button>
+      ) : (
+        <Button>Connect bank</Button>
+      )}
+    </>
+  );
+};
 
-export default PlaidLink
+export default PlaidLink;
