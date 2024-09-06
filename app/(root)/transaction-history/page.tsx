cc74a108574324a9d7cecc12fb3f6b748a861b8e
AlbertGorgori <albert.gorgori@outlook.com>
2024-09-06 11:20:08 +0200

    Transaction history

--- app/(root)/transaction-history/page.tsx
+++ app/(root)/transaction-history/page.tsx
@@ -1,6 +1,8 @@
 import HeaderBox from "@/components/HeaderBox";
+import TransactionsTable from "@/components/TransactionsTable";
 import { getAccount, getAccounts } from "@/lib/actions/bank.actions";
 import { getLoggedInUser } from "@/lib/actions/user.actions";
+import { formatAmount } from "@/lib/utils";
 import React from "react";
 
 const TransactionHistory = async ({
@@ -18,8 +20,9 @@ const TransactionHistory = async ({
   const appwriteItemId = (id as string) || accountsData[0]?.appwriteItemId;
 
   const account = await getAccount({ appwriteItemId });
+  console.log("Account", account);
   return (
-    <div className="transaction">
+    <div className="transactions">
       <div className="transaction-header">
         <HeaderBox
           title="Transaction History"
@@ -30,9 +33,27 @@ const TransactionHistory = async ({
       <div className="space-y-6">
         <div className="transactions-account">
           <div className="flex flex-col gap-2">
-            <h2></h2>
+            <h2 className="text-18 font-bold text-white">
+              {account?.data?.name}
+            </h2>
+            <p className="text-14 text-blue-25">
+              {account?.data?.officialName}
+            </p>
+            <p className="text-14 font-semibold tracking-[1.1px] text-white">
+              ●●● ●●●● ●●●●{" "}
+              <span className="text-16">{account?.data?.mask}</span>
+            </p>
+          </div>
+          <div className="transactions-account-balance">
+            <p className="text-14">Current Balance</p>
+            <p className="text-24 text-center font-bold">
+              {formatAmount(account?.data?.currentBalance)}
+            </p>
           </div>
         </div>
+        <section className="flex w-full flex-col gap-6">
+          <TransactionsTable transactions={account?.transactions} />
+        </section>
       </div>
     </div>
   );
