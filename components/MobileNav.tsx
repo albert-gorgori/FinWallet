7248e97736c0ae41f21c543cb2592018a0fff280
AlbertGorgori <albert.gorgori@outlook.com>
2024-09-07 07:43:11 +0200

    Adjustments

--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -1,4 +1,5 @@
 "use client";
+
 import {
   Sheet,
   SheetClose,
@@ -8,24 +9,27 @@ import {
   SheetTitle,
   SheetTrigger,
 } from "@/components/ui/sheet";
+import { sidebarLinks } from "@/constants";
+import { cn } from "@/lib/utils";
 import Image from "next/image";
 import Link from "next/link";
-import { cn } from "@/lib/utils";
-import { sidebarLinks } from "@/constants";
 import { usePathname } from "next/navigation";
 import Footer from "./Footer";
+import PlaidLink from "./PlaidLink";
 
 const MobileNav = ({ user }: MobileNavProps) => {
   const pathname = usePathname();
+
   return (
-    <section className="w-full max-w-[264px]">
+    <section className="w-fulll max-w-[264px]">
       <Sheet>
         <SheetTrigger>
           <Image
             src="/icons/hamburger.svg"
             width={30}
             height={30}
-            alt="menu icon"
+            alt="menu"
+            className="cursor-pointer"
           />
         </SheetTrigger>
         <SheetContent side="left" className="border-none bg-white">
@@ -37,7 +41,7 @@ const MobileNav = ({ user }: MobileNavProps) => {
               src="/icons/logo.svg"
               width={34}
               height={34}
-              alt="FinWallet logo"
+              alt="Horizon logo"
             />
             <h1 className="text-26 font-ibm-plex-serif font-bold text-black-1">
               Fin Wallet
@@ -45,24 +49,24 @@ const MobileNav = ({ user }: MobileNavProps) => {
           </Link>
           <div className="mobilenav-sheet">
             <SheetClose asChild>
-              <nav className="flex h-full flex-col gap-4 pt-16 text-white">
-                {sidebarLinks.map((link, index) => {
+              <nav className="flex h-full flex-col gap-6 pt-16 text-white">
+                {sidebarLinks.map((item) => {
                   const isActive =
-                    pathname === link.route ||
-                    pathname.startsWith(`${link.route}/`);
+                    pathname === item.route ||
+                    pathname.startsWith(`${item.route}/`);
 
                   return (
-                    <SheetClose asChild key={link.route}>
+                    <SheetClose asChild key={item.route}>
                       <Link
-                        key={index}
-                        href={link.route}
+                        href={item.route}
+                        key={item.label}
                         className={cn("mobilenav-sheet_close w-full", {
                           "bg-bank-gradient": isActive,
                         })}
                       >
                         <Image
-                          src={link.imgURL}
-                          alt={link.label}
+                          src={item.imgURL}
+                          alt={item.label}
                           width={20}
                           height={20}
                           className={cn({
@@ -70,22 +74,22 @@ const MobileNav = ({ user }: MobileNavProps) => {
                           })}
                         />
                         <p
-                          className={cn(" text-16 font-semibold text-black-2", {
+                          className={cn("text-16 font-semibold text-black-2", {
                             "text-white": isActive,
                           })}
                         >
-                          {link.label}
+                          {item.label}
                         </p>
                       </Link>
                     </SheetClose>
                   );
                 })}
 
-                USER
+                <PlaidLink user={user} />
               </nav>
             </SheetClose>
 
-            <Footer user={user} type='mobile' />
+            <Footer user={user} type="mobile" />
           </div>
         </SheetContent>
       </Sheet>
